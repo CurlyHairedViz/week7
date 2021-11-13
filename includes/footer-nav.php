d597dbaad35b155b3c797ea5cd3be34346537546
CurlyHairedViz <69843846+CurlyHairedViz@users.noreply.github.com>
2021-11-13 14:59:43 -0500

    First commit

---
+++ includes/footer-nav.php
@@ -0,0 +1,7 @@
+<footer>   
+    <!-- Fine print navigation -->         
+    <nav>
+        <a href="/~Viraj200499983/week-7/about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | <a href="/~Viraj200499983/week-7/about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> | <a href="/~Viraj200499983/week-7/about/terms-of-use.php" title="Understand our Terms of Use">Terms of Use</a> | <a href="/~Viraj200499983/week-7/about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained">Limitation of Liability</a>
+    </nav>
+    <p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
+</footer>
